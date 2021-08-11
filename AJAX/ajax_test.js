4ce30d81285bcff14a3be49f7672736c11a07122
cuberJ <2113159231@qq.com>
2021-08-12 00:38:45 +0800

    8.11 解决了cores跨域的问题

--- AJAX/ajax_test.js
+++ AJAX/ajax_test.js
@@ -1,8 +1,11 @@
 const express = require('express');
 const app = express();
 app.get('/server', (request, response)=>{
-    response.send('hello world')
-    response.setHeader('Access-Control-Allow-Origin', '*')
+    response.setHeader('Access-Control-Allow-Origin', '*'); //这里一个非常重要的点：setHeader的操作一定要在其他操作的前面，否则会导致浏览器跨域请求失败，控制台会报错No 'Access-Control-Allow-Origin' header is present on the requested resource.
+    response.setHeader('Access-Control-Allow-Methods', '*');
+    //response.send('hello world')
+    response.send('Now This is the second test...')
+    
 });
 
 app.listen(8000, ()=>{
